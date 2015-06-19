5d348af86d173199cf38b89297c56d342c5db58e
Jay Mundrawala <jdmundrawala@gmail.com>
2015-06-18 21:26:55 -0700

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'windows-sdk'
+maintainer 'Chef Software'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs the Windows 8.1 SDK'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.1'
