811f25c8677733364abfa4da8f4c86f85feffbe4
Jay Mundrawala <jdmundrawala@gmail.com>
2015-06-23 08:40:50 -0700

    Prepare 1.0.0 release

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name 'windows-sdk'
 maintainer 'Chef Software'
-maintainer_email 'cookbooks@chef.io'
+maintainer_email 'jdm@chef.io'
 license 'Apache 2.0'
 description 'Installs the Windows 8.1 SDK'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+
+supports 'windows'
+
+source_url 'https://github.com/jdmundrawala/windows-sdk-cookbook'
+issues_url 'https://github.com/jdmundrawala/windows-sdk-cookbook/issues'
+
+version '1.0.0'
